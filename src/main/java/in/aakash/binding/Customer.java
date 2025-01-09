c69ea77f6dae66e9dd9dbec6c8fe14f42520558f
Aakash Patel <aakashpatelsky11@gmail.com>
2025-01-09 22:01:00 +0530

    Get & Post method Test using Postman in details concept

--- src/main/java/in/aakash/binding/Customer.java
+++ src/main/java/in/aakash/binding/Customer.java
@@ -1,30 +1,42 @@
 package in.aakash.binding;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+import lombok.Data;
+
+@XmlRootElement
 public class Customer {
-
 	
 	private String name;
 	private String gmail;
 	private String gender;
 	
+	@XmlElement
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	@XmlElement
 	public String getGmail() {
 		return gmail;
 	}
 	public void setGmail(String gmail) {
 		this.gmail = gmail;
 	}
+	@XmlElement
 	public String getGender() {
 		return gender;
 	}
 	public void setGender(String gender) {
 		this.gender = gender;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", gmail=" + gmail + ", gender=" + gender + "]";
 	}
 	
 	
--- src/main/java/in/aakash/rest/CustomerRestController.java
+++ src/main/java/in/aakash/rest/CustomerRestController.java
@@ -1,21 +1,41 @@
 package in.aakash.rest;
 
-import java.beans.Customizer;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import in.aakash.binding.Customer;
 
 @RestController
 public class CustomerRestController {
-	@GetMapping("/customer")
-	public Customer getCustomer()
-	{
-		Customer c= new Customer();
+	
+	public CustomerRestController() {
+		System.out.println("Custome Rest :: Constructor Called...!!");
+	}
+	
+	@GetMapping(
+			value = "/customer" ,
+			produces = { "application/xml", "application/json"}
+	)
+	public Customer getCustomer() throws Exception{
+		Customer c = new Customer();
 		c.setGender("Male");
 		c.setName("Jhon");
 		c.setGmail("John12@gmail.com");
 		return c;
+	}
+	
+	@PostMapping(
+			   value = "/customer",
+			   consumes = { "application/json", "application/xml"},
+			   produces = {"text/plain"}
+			)
+	public ResponseEntity<String> addCustomer(@RequestBody Customer customer)
+	{
+		System.out.println(customer);
+		// logic to save customer data in the database table
+		return new ResponseEntity<>("saved Customer",HttpStatus.CREATED);
 	}
 }
